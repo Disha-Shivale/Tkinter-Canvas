4506ab784d351ad85dd54324a43c8f207290b87b
Disha-Shivale <dishashivale712@gmail.com>
2021-05-02 22:42:29 +0500

    first commit

---
+++ canvasbasic.py
@@ -0,0 +1,8 @@
+import tkinter
+from tkinter import *
+
+m = Tk()
+c = Canvas(m, bg="RED", height="200", width="300")
+c.pack()
+
+m.mainloop()
---
+++ tkintercanvas.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter import *
+
+m = Tk()
+m.geometry("300x300")
+c = Canvas(m,bg = "RED", height="200", width = "200")
+c.pack()
+
+m.mainloop()
